ca9acc589df7f4aa9d55630666e9e4cad2f189f4
ritvik144 <96761610+ritvik144@users.noreply.github.com>
2022-01-20 00:10:00 +0530

    Add files via upload

---
+++ assignment 2 output.py
@@ -0,0 +1,67 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:/Users/91981/AppData/Local/Programs/Python/Python310/xyz.py ====
+Q1
+
+part a
+Length of the string is :  35
+
+part b
+ The string in reverse order is : egaugnal evitisnes esac a si nohtyP
+
+part c
+The sliced string is: a case sensitive
+
+part d
+The new string is :  Python is object oriented language
+
+part e 
+10
+
+part f
+Pythonisacasesensitivelanguage
+
+Q2
+Hey, Ritvik Arora here! 
+My SID is 21105033 
+I am from ECE department and my CGPA is 9 
+
+Q3
+
+part a 
+ a&b :  8
+
+part b
+ a|b :  58
+
+part c
+ a^b :  50
+
+part d
+Left shift a by 2 bits : 224
+
+ Left shift b by 2 bits : 40
+
+part e
+Right shift a by 2 bits : 14
+Right shift b by 4 bits : 160
+
+Q4
+enter first no. 45
+enter second no. 43
+enter third no. 68
+ The largest no. is  68
+
+Q5
+Enter the string My name is Ritvik
+yes
+
+Q6
+
+Enter length of first side: 4
+
+Enter length of second side: 6
+
+Enter length of third side: 8
+Yes, triangle can be formed with these values
