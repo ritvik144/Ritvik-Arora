7736cc2c84d913079001c19f6f9f2cc7c49ec6e8
ritvik144 <96761610+ritvik144@users.noreply.github.com>
2022-02-28 12:58:32 +0530

    Add files via upload

---
+++ assgn 4 output.py
@@ -0,0 +1,81 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/91981/OneDrive - 123/Desktop/Intro to computing/assgn 4.py =
+Q1
+Enter number of disk in tower of Hanoi: 3
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Destination
+Move disk 2 from Source Tower to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source Tower to Destination
+Move disk 1 from Auxiliary to Source Tower
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source Tower to Destination
+Q2
+enter the no. of rows 9
+          1 
+         1 1 
+        1 2 1 
+       1 3 3 1 
+      1 4 6 4 1 
+     1 5 10 10 5 1 
+    1 6 15 20 15 6 1 
+   1 7 21 35 35 21 7 1 
+  1 8 28 56 70 56 28 8 1 
+Q3
+
+Enter an Integer: 76
+Enter another Integer:54
+
+The quotient when 76 is divided by 54 is 1.
+The remainder when 76 is divided by 54 is 22.
+
+Que3.a
+Function is Not-callable
+
+Que3.b
+All result values are 'non-zero'
+
+Que3.c
+Appended (4,5,6) in the values (1,22)
+Values greater than 4 is 22,5,6
+
+Que3.d
+Above result in set form is shown below:
+{5, 22, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 22
+Hash value of 22 is 22
+Q4
+enter name of student : ritvik 
+Enter SID of  ritvik : 21105033
+object created
+
+The name of the student is  ritvik  and his/her roll number is 21105033
+object deleted
+Q5
+The data is stored where:
+Mehak has a salary of 40000 rupees
+Ashok has a salary of 50000 rupees
+Viren has a salary of 60000 rupees
+All deleted, i.e. Viren
+
+Q6
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name: ritvik
+Enter Player2 name: aryan
+
+Enter Word by  ritvik: stop
+Enter Word by  aryan: post
+
+Friendship of  ritvik and  aryan is REAL
